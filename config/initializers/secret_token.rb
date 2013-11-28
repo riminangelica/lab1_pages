73e78b3b7ec9559efd507d948e4508451ad4848f
Rimina Angelica <riminarchive@gmail.com>
2013-11-28 22:00:11 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lab1Pages::Application.config.secret_key_base = '6b742b65832c039a39927c0c6a65d8cbdf80ed5966a46c1e94ab40779e06db9ee8ddcef50af1fbfe98f08d739bb0200b8d7cd72bd75177f342f878ffc55e1c7d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lab1Pages::Application.config.session_store :cookie_store, key: '_lab1_pages_session'
